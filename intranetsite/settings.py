3a36bd7671b8367ad6e304e693ed7118b1bacf53
John Njenga <joninduati31@gmail.com>
2021-01-24 08:02:19 +0300

    Added fa-brands-400.eot to webfonts

--- intranetsite/settings.py
+++ intranetsite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-mediastore-site.herokuapp.com', 'localhost']
 
 # Application definition
 
