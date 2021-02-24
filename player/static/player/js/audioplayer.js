dcb70a773c7d255529da460f182949168f39d6e7
John Njenga <joninduati31@gmail.com>
2021-02-24 17:55:55 +0300

    Added a footer to the site

--- player/static/player/js/audioplayer.js
+++ player/static/player/js/audioplayer.js
@@ -170,3 +170,49 @@ parser(sources);
 // document.getElementById('sourceUrl').value = "http://localhost:8000/media/music/Blake_Shelton_-_Happy_Anywhere_feat._Gwen_Stefani_Official_Music_Video.opus";
 // addList();
 // document.getElementById("glow").classList.remove("disable-animation");
+
+const darkToggler = document.getElementById('dark-theme')
+const fanDivs = document.querySelectorAll('.fan')
+const formFields = document.querySelectorAll('.form-control')
+
+const changeTheme = (value) =>{
+    if (parseInt(value) === 0){
+        fanDivs.forEach(div =>{
+            div.classList.remove('bg-dark')
+            div.classList.add('bg-light')
+            document.body.style.backgroundColor = 'antiquewhite'
+            document.body.style.color = '#000'
+        })
+        formFields.forEach(field =>{
+            field.classList.remove('bg-dark')
+            field.classList.add('text-dark')
+            field.classList.remove('text-white')
+        })      
+        darkToggler.style.border = '1px solid antiquewhite'
+    }else if (parseInt(value) === 1){
+        fanDivs.forEach(div =>{
+            div.classList.remove('bg-light')
+            div.classList.add('bg-dark')
+            document.body.style.backgroundColor = 'rgb(30, 35, 39, 0.911)'
+            document.body.style.color = '#fff'
+        })
+        formFields.forEach(field =>{
+            field.classList.add('bg-dark')
+            field.classList.remove('text-dark')
+            field.classList.add('text-white')
+        })
+    }
+}
+
+const data = localStorage.getItem('dark-theme')
+if (data){
+    darkToggler.value = data 
+    changeTheme(darkToggler.value)
+}else{
+    changeTheme(1)
+}
+
+darkToggler.onchange = (e) => {
+    localStorage.setItem('dark-theme', e.target.value)
+    changeTheme(e.target.value)
+}
