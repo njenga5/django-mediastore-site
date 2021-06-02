0af564f862536a78cfc68da94333b686933fe0c1
njenga5 <60683527+njenga5@users.noreply.github.com>
2021-06-03 01:03:51 +0300

    updated settings

--- intranetsite/settings.py
+++ intranetsite/settings.py
@@ -164,7 +164,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 
